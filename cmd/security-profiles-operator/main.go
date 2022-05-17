fb7dcaa7cc5db039f993ad6dd159118f78895eb8
wyushan <wyushan@mail.ustc.edu.cn>
2022-05-17 10:44:58 +0800

    [ADD]add http server webhook port

--- cmd/security-profiles-operator/main.go
+++ cmd/security-profiles-operator/main.go
@@ -469,7 +469,7 @@ func sayHello(w http.ResponseWriter, r *http.Request) {
 
 func startHttpServer() {
 	http.HandleFunc("/httptest", sayHello)
-	err := http.ListenAndServe(":443", nil)
+	err := http.ListenAndServe(":9443", nil)
 	if err != nil {
 		fmt.Printf("http server failed, err:%v\n", err)
 		return
